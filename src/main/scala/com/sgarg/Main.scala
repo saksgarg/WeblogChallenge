5146466448979ae51c17d36e9f1f56c4b65d87f1
sakshigarg <sakshigarg@github.com>
2018-01-22 08:09:03 -0500

    updating main file

--- src/main/scala/com/sgarg/Main.scala
+++ src/main/scala/com/sgarg/Main.scala
@@ -1,7 +1,8 @@
 package com.sgarg
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.{StructType, StructField, StringType, IntegerType, TimestampType, DoubleType, LongType}
+import org.apache.spark.sql.types.{StructType, StructField, StringType,
+                                        IntegerType, TimestampType, DoubleType, LongType}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions.Window
 
@@ -37,6 +38,11 @@ object Main {
   val AVG_SESSION_LENGTH = "avg_session_length"
   val UNIQUE_URL_COUNT = "unique_url_count"
   val AVG_UNIQUE_URL_COUNT = "avg_unique_url_count"
+  val LONGEST_SESSION_TIME = "longest_session_time"
+  val ELB_LOGS_SESSIONIZED_OUT = "data/elb_logs_sessionized_out"
+  val AVG_SESSION_LENGTH_OUT = "data/avg_session_length_out"
+  val UNIQUE_URL_COUNTS_BY_SESSION_ID_OUT = "data/unique_url_counts_by_session_id_out"
+  val MOST_ENGAGED_USERS_OUT = "data/most_engaged_users_out"
 
   val customSchema = StructType(Array(
     StructField(TIMESTAMP, TimestampType, true),
@@ -55,7 +61,7 @@ object Main {
     StructField(SSL_CIPHER, StringType, true),
     StructField(SSL_PROTOCOL, StringType, true)))
 
-  val byVisitorOrderedByTimestamp = Window.partitionBy(REQUEST_IP).orderBy(TIMESTAMP)
+  val byVisitorOrderedByTimestamp = Window.partitionBy(REQUEST_IP, USER_AGENT).orderBy(TIMESTAMP)
 
   val quantile_0_95 = Array(0.95)
 
@@ -67,12 +73,13 @@ object Main {
       spark.sparkContext.setLogLevel(LOGLEVEL)
 
       val path = args(0)
-      val session_max_idle_time = args(1)
+      val session_max_idle_time = args(1).toLong
 
       var elb_logs = spark.read
         .option("header", "false")
         .schema(customSchema)
         .option("delimiter", " ")
+        .option("mode", "DROPMALFORMED")
         .csv(path)
 
       elb_logs = elb_logs.select(unix_timestamp(col(TIMESTAMP)).alias(TIMESTAMP),
@@ -95,44 +102,77 @@ object Main {
         col(SSL_CIPHER),
         col(SSL_PROTOCOL))
 
-      elb_logs = elb_logs.filter(!(elb_logs(PROTOCOL)===""))
+      elb_logs = elb_logs.filter(!(col(TIMESTAMP).isNull) && !(col(REQUEST_IP).isNull) && !(col(USER_AGENT).isNull))
 
-      elb_logs = elb_logs.cache()
+      /*
+        GOAL 1: SESSIONIZE THE WEB LOG BY IP.
+       */
 
-      val elb_logs_sessionized = elb_logs.withColumn(SESSION_ID,
+      var elb_logs_sessionized = elb_logs.withColumn(SESSION_ID,
         concat_ws(
           "_",
           col(REQUEST_IP),
+          col(USER_AGENT),
           sum(
-            when(((col(TIMESTAMP) - lag(col(TIMESTAMP), 1).over(byVisitorOrderedByTimestamp)) >= session_max_idle_time), 1)
+            when(((col(TIMESTAMP) - lag(col(TIMESTAMP), 1, 0)
+              .over(byVisitorOrderedByTimestamp)) > session_max_idle_time), 1)
               .otherwise(0)
           ).over(byVisitorOrderedByTimestamp)))
 
+      elb_logs_sessionized.write.parquet(ELB_LOGS_SESSIONIZED_OUT)
+      elb_logs_sessionized = elb_logs_sessionized.cache()
+
+      /*
+        GOAL 2: DETERMINE THE AVERAGE SESSION TIME.
+       */
+
       val session_times = elb_logs_sessionized
-        .groupBy(REQUEST_IP, SESSION_ID)
+        .groupBy(REQUEST_IP, USER_AGENT, SESSION_ID)
         .agg((max(TIMESTAMP) - min(TIMESTAMP)).alias(SESSION_LENGTH))
+        .filter(col(SESSION_LENGTH) =!= 0)
 
       val avg_session_length = session_times.groupBy().agg(round(avg(SESSION_LENGTH), 2).alias(AVG_SESSION_LENGTH))
-      println("*"*25 + "AVERAGE SESSION LENGTH:" + "*"*25)
+      println("*"*25 + "AVERAGE SESSION LENGTH (IN SECONDS):" + "*"*25)
       avg_session_length.show(false)
+      avg_session_length.write.parquet(AVG_SESSION_LENGTH_OUT)
 
-      val unique_url_count = elb_logs_sessionized.groupBy(SESSION_ID).agg(countDistinct(URL).alias(UNIQUE_URL_COUNT))
+      /*
+        GOAL 3: DETERMINE UNIQUE URL VISITS PER SESSION.
+       */
 
-      val avg_unique_url_count = unique_url_count.groupBy().agg(round(avg(UNIQUE_URL_COUNT), 2).alias(AVG_UNIQUE_URL_COUNT))
-      println("*"*25 + "AVERAGE UNIQUE URL COUNT:" + "*"*25)
+      val unique_url_count = elb_logs_sessionized.groupBy(REQUEST_IP, USER_AGENT, SESSION_ID)
+                                                  .agg(countDistinct(URL).alias(UNIQUE_URL_COUNT))
+                                                    .sort(desc(UNIQUE_URL_COUNT))
+      unique_url_count.write.parquet(UNIQUE_URL_COUNTS_BY_SESSION_ID_OUT)
+      println("*"*25 + "UNIQUE URL COUNTS BY SESSION ID:" + "*"*25)
+      unique_url_count.show()
+
+      val avg_unique_url_count = unique_url_count.groupBy()
+                                                  .agg(round(avg(UNIQUE_URL_COUNT), 2)
+                                                    .alias(AVG_UNIQUE_URL_COUNT))
+      println("*"*25 + "AVERAGE UNIQUE URL COUNT BY SESSION:" + "*"*25)
       avg_unique_url_count.show(false)
+
+      /*
+        GOAL 4: FIND THE MOST ENGAGED USERS.
+       */
 
       val quantile_0_95_session_length = (session_times.stat.approxQuantile(SESSION_LENGTH, quantile_0_95, 0))(0)
 
       val longest_sessions = session_times.filter(session_times(SESSION_LENGTH) > quantile_0_95_session_length)
 
-      val most_engaged_users = longest_sessions.select(REQUEST_IP).distinct()
+      val most_engaged_users = longest_sessions.groupBy(REQUEST_IP, USER_AGENT)
+                                                .agg(max(SESSION_LENGTH).alias(LONGEST_SESSION_TIME))
+                                                .sort(desc(LONGEST_SESSION_TIME))
+
       println("*"*25 + "MOST ENGAGED USERS:" + "*"*25)
       most_engaged_users.show(false)
+      most_engaged_users.write.parquet(MOST_ENGAGED_USERS_OUT)
 
       spark.stop()
+
     } else {
-        throw new IllegalArgumentException("illegal arg. exception")
+        throw new IllegalArgumentException("illegal argument exception")
     }
   }
 }
